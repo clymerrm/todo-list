cb8e409ecf14a9765781c8315123b942b2096c63
Rick Clymer <clymerrm@gmail.com>
2019-09-20 08:16:49 -0400

    base unittest framework

--- frontend_tests/python/navigate_to_links.py
+++ frontend_tests/python/navigate_to_links.py
@@ -1,17 +1,30 @@
-from webdriver_manager.chrome import ChromeDriverManager
+import unittest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 from frontend_tests.python.pages import *
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-base = Base(driver)
-header = Header(driver)
 
-base.get_base_page()
-header.click_schedule()
-assert 'Schedule' in header.return_page_title()
+class LinkTest(unittest.TestCase):
 
-base.get_base_page()
-header.click_speakers()
-assert 'Speaker' in header.return_page_title()
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.base = Base(self.driver)
+        self.header = Header(self.driver)
 
-base.quit_driver()
+    def tearDown(self):
+        self.base.quit_driver()
+
+    def test_clicking_schedule_link(self):
+        self.base.get_base_page()
+        self.header.click_schedule()
+        assert 'Schedule' in self.header.return_page_title()
+
+    def test_clicking_speakers_link(self):
+        self.base.get_base_page()
+        self.header.click_speakers()
+        assert 'Speakers' in self.header.return_page_title()
+
+
+if __name__ == "__main__":
+    unittest.main()
