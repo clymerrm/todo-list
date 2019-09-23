c1a5e52e8b8dc21d2fa5bab266e7ee63673b9e67
Rick Clymer <clymerrm@gmail.com>
2019-09-23 03:49:55 -0400

    Tweaking js page object

---
+++ frontend_tests/javascript/pageobjects/base.po.js
@@ -0,0 +1,40 @@
+const {webdriver, Builder, until, By} = require('selenium-webdriver');
+
+const BasePage = function() {
+
+    this.driver = new Builder()
+        .forBrowser('chrome')
+        .usingServer('http://localhost:4444/wd/hub')
+        .build();
+
+    this.baseUrl = 'http://localhost:3000';
+
+    this.goToPage = async function() {
+        await this.driver.get(this.baseUrl);
+    };
+
+    this.quitDriver = function() {
+        this.driver.quit()
+    };
+
+    this.titleContains = function(titleContents) {
+        return this.driver.wait(until.titleContains(titleContents))
+    };
+
+    this.findByXpath = function(xpath) {
+        this.driver.wait(until.elementLocated(By.xpath(xpath)), 10000);
+        return this.driver.findElement(By.xpath(xpath));
+    };
+
+    this.findByCss = function(css) {
+        this.driver.wait(until.elementLocated(By.css(css)), 10000);
+        return this.driver.findElement(By.css(css));
+    };
+
+    this.clickElement = function(element) {
+        element.click();
+    };
+
+}
+
+module.exports = BasePage;
